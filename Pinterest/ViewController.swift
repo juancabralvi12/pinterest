d366d0be67f64d0b61238362cff05161929044c4
Juan Cabral <JuanCabral@MacBook-Pro-de-Juan.local>
2019-01-31 20:59:34 -0600

    Initial commit for our oustanding Pinterest application

--- Pinterest/ViewController.swift
+++ Pinterest/ViewController.swift
@@ -9,12 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        
+        //add subview
+        view.addSubview(inputContainerView)
+        view.addSubview(firstButton)
+        inputContainerView.addSubview(nameTextField)
+        
+        
+        //constraints
+        // constraints for input
+        
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        
+        
+        
+        firstButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 20).isActive = true
+        firstButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        firstButton.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        firstButton.rightAnchor.constraint(equalTo: inputContainerView.rightAnchor).isActive = true
+    
+        nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    let nameTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Name"
+        tf.backgroundColor = .white
+        return tf
+    }()
+    
+    let inputContainerView : UIView =  {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let firstButton : UIButton = {
+       let ub = UIButton()
+        ub.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        ub.setTitle("Register", for: .normal)
+        ub.translatesAutoresizingMaskIntoConstraints = false
+        return ub
+    }()
 
 
 }
 
+
+extension UIColor {
+    
+    convenience init(r:CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+}
